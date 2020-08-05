772ab5e69765a6f3baf49ca25357ebc0d3a7dd1a
Karasuma1412 <bjoerninorek@gmail.com>
2020-08-05 07:48:19 +0200

    allow monitoring of capnzero applications inside the cluster from outside the cluster

--- capnzero/src/monitor.cpp
+++ capnzero/src/monitor.cpp
@@ -8,18 +8,26 @@
 
 //#define DEBUG_SENDER
 
-void* pairSocket;
+void* monitor_socket;
 
 void callbackCapnProto(std::string msg)
 {
     std::cout << "Called callback..." << std::endl;
     std::cout << "Message type: CapnProto" << std::endl;
-    std::cout << msg << std::endl;
 
-    zmq_msg_t message;
-    zmq_send(pairSocket, strdup(msg.c_str()),
-             strlen(msg.c_str()), ZMQ_DONTWAIT);
-    zmq_msg_close(&message);
+    zmq_msg_t zmq_msg;
+    zmq_msg_init(&zmq_msg);
+    std::cout << "wait for request..." << std::endl;
+    int rc = zmq_msg_recv(&zmq_msg, monitor_socket, 0);
+    if (rc != -1) {
+        std::cout << "received request" << std::endl;
+        std::string msg_str(static_cast<char*>(zmq_msg_data(&zmq_msg)), zmq_msg_size(&zmq_msg));
+        std::cout << msg_str << std::endl;
+        zmq_send(monitor_socket, msg.c_str(), msg.length(), 0);
+    } else {
+        std::cerr << "not received request: error code " << zmq_errno() << std::endl;
+    }
+    zmq_msg_close(&zmq_msg);
 }
 
 static bool interrupted = false;
@@ -41,31 +49,33 @@ static void s_catch_signals(void)
 int main(int argc, char** argv)
 {
     s_catch_signals();
-
     for (size_t i = 0; i < argc; i++) {
         std::cout << "Param " << i << ": '" << argv[i] << "'" << std::endl;
     }
 
-    std::cout << "Selected encoding: CapnProto" << std::endl;
     void* ctx = zmq_ctx_new();
+    monitor_socket = zmq_socket(ctx, ZMQ_REP);
+    int timeout = 1000;
+    zmq_setsockopt(monitor_socket, ZMQ_SNDTIMEO, &timeout, sizeof(int));
+    zmq_setsockopt(monitor_socket, ZMQ_RCVTIMEO, &timeout, sizeof(int));
 
-    pairSocket = zmq_socket(ctx, ZMQ_PAIR);
-    zmq_bind(pairSocket, "tcp://127.0.0.1:45678");
+    int rc = zmq_bind(monitor_socket, "tcp://*:12345");
+    if (rc == 1) {
+        std::cerr << "error connecting to address tcp://*:12345"
+                  << ": error code " << zmq_errno() << std::endl;
+        return -1;
+    }
 
     auto sub = new capnzero::SubscriberCapnProto(ctx, capnzero::Protocol::UDP);
     sub->setTopic("monitoring");
-
-//    sub->addAddress("@capnzero.ipc");
     sub->addAddress("224.0.0.2:9876");
-//    sub->addAddress("127.0.0.1:5555");
-
     sub->subscribe(&callbackCapnProto);
     while (!interrupted) {
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
     delete sub;
-
+    free(monitor_socket);
     zmq_ctx_term(ctx);
     return 0;
 }
