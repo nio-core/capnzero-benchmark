c06e9427265c89b58b95f3151721ba7c58f2fc54
Karasuma1412 <bjoerninorek@gmail.com>
2020-08-11 16:03:25 +0200

    use pub/sub zeromq sockets for publishing monitor messages

--- capnzero/msg/string.capnp
+++ capnzero/msg/string.capnp
@@ -3,14 +3,13 @@ $Cxx.namespace("capnzero");
 
 @0xd9693c54d5510668;
 
-# UUID (64 bit)
-# Message Info Message (var)
-# Status int(64 bit)
-# Liste von Zustandsänderung (int) (var)
-
 struct MessageCapnp {
     id @0 :Text;
     status @2 :Int64;
     messageInfo @1 :Text;
     states @3 :List(Int64);
+}
+
+struct String {
+  string @0 :Text;
 }
--- capnzero/src/monitor.cpp
+++ capnzero/src/monitor.cpp
@@ -15,19 +15,12 @@ void callbackCapnProto(std::string msg)
     std::cout << "Called callback..." << std::endl;
     std::cout << "Message type: CapnProto" << std::endl;
 
-    zmq_msg_t zmq_msg;
-    zmq_msg_init(&zmq_msg);
-    std::cout << "wait for request..." << std::endl;
-    int rc = zmq_msg_recv(&zmq_msg, monitor_socket, 0);
-    if (rc != -1) {
-        std::cout << "received request" << std::endl;
-        std::string msg_str(static_cast<char*>(zmq_msg_data(&zmq_msg)), zmq_msg_size(&zmq_msg));
-        std::cout << msg_str << std::endl;
-        zmq_send(monitor_socket, msg.c_str(), msg.length(), 0);
-    } else {
-        std::cerr << "not received request: error code " << zmq_errno() << std::endl;
-    }
-    zmq_msg_close(&zmq_msg);
+    const char *message = msg.c_str();
+    zmq_msg_t out_msg;
+    zmq_msg_init_size(&out_msg, strlen(message));
+    memcpy(zmq_msg_data(&out_msg), message, strlen(message));
+    zmq_msg_send(&out_msg, monitor_socket, ZMQ_DONTWAIT);
+    zmq_msg_close(&out_msg);
 }
 
 static bool interrupted = false;
@@ -53,18 +46,10 @@ int main(int argc, char** argv)
         std::cout << "Param " << i << ": '" << argv[i] << "'" << std::endl;
     }
 
-    void* ctx = zmq_ctx_new();
-    monitor_socket = zmq_socket(ctx, ZMQ_REP);
-    int timeout = 1000;
-    zmq_setsockopt(monitor_socket, ZMQ_SNDTIMEO, &timeout, sizeof(int));
-    zmq_setsockopt(monitor_socket, ZMQ_RCVTIMEO, &timeout, sizeof(int));
-
-    int rc = zmq_bind(monitor_socket, "tcp://*:12345");
-    if (rc == 1) {
-        std::cerr << "error connecting to address tcp://*:12345"
-                  << ": error code " << zmq_errno() << std::endl;
-        return -1;
-    }
+    void *ctx = zmq_init(1);
+    monitor_socket = zmq_socket(ctx, ZMQ_PUB);
+    zmq_bind(monitor_socket, "tcp://*:12345");
+    zmq_sleep(1);
 
     auto sub = new capnzero::SubscriberCapnProto(ctx, capnzero::Protocol::UDP);
     sub->setTopic("monitoring");
